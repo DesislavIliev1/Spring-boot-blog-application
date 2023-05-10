785968fcef04e7f4f66b738ed37306c1748c2116
DesislavIliev1 <desislav.iliev12@abv.bg>
2023-05-10 08:25:04 +0300

    Implementing new methods
    Implementing getAllPosts and getPostById

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/service/impl/PostServiceimpl.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/service/impl/PostServiceimpl.java
@@ -1,10 +1,14 @@
 package com.springboot.blog.service.impl;
 
 import com.springboot.blog.entity.Post;
+import com.springboot.blog.exeption.ResourceNotFoundException;
 import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.repository.PostRepository;
 import com.springboot.blog.service.PostService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PostServiceimpl implements PostService {
@@ -20,21 +24,45 @@ public class PostServiceimpl implements PostService {
     public PostDto createPost(PostDto postDto) {
 
         //convert Dto to Entity
+        Post post = mapToEntity(postDto);
+        Post newpost = postRepository.save(post);
+
+        //convert Entity to Dto
+        PostDto postResponse = mapToDto(newpost);
+
+        return postResponse;
+    }
+
+    @Override
+    public List<PostDto> getAllPosts() {
+        List<Post> posts = postRepository.findAll();
+        return posts.stream().map(post -> mapToDto(post)).collect(Collectors.toList());
+
+    }
+
+    @Override
+    public PostDto getPostById(long id) {
+        Post post = postRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("post", "id",id));
+        return mapToDto(post);
+    }
+
+    //convert Entity to Dto
+    private PostDto mapToDto(Post post){
+        PostDto postDto = new PostDto();
+        postDto.setId(post.getId());
+        postDto.setTitle(post.getTitle());
+        postDto.setDescription(post.getDescription());
+        postDto.setContent(post.getContent());
+        return  postDto;
+    }
+
+    //Convert Dto to Entity
+    private Post mapToEntity(PostDto postDto){
         Post post = new Post();
         post.setTitle(postDto.getTitle());
         post.setDescription(postDto.getDescription());
         post.setContent(postDto.getContent());
-
-        Post newpost = postRepository.save(post);
-
-        //convert Entity to Dto
-
-        PostDto postResponse = new PostDto();
-        postResponse.setId(newpost.getId());
-        postResponse.setTitle(newpost.getTitle());
-        postResponse.setDescription(newpost.getDescription());
-        postResponse.setContent(newpost.getContent());
-
-        return postResponse;
+        return  post;
     }
+
 }
