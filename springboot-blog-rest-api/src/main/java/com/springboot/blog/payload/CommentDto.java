4b791ce58c123f5825ce95b23c54e8d4c1182645
DesislavIliev1 <desislav.iliev12@abv.bg>
2023-05-19 07:26:19 +0300

    implementing basic authentication
    implementing basic authentication using spring security

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/payload/CommentDto.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -1,12 +1,20 @@
 package com.springboot.blog.payload;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CommentDto {
     private long id;
 
+    @NotEmpty(message = "Name should not be empty or null")
     private String name;
+    @NotEmpty(message = "Email should not be empty or null")
+    @Email
     private String email;
+    @NotEmpty(message = "Body should not be empty or null")
+    @Size(min = 10, message = "Comment body should be minimum 10 characters")
     private String body;
 }
